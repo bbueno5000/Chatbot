7d0288262135667da52b9c0242141a17af860a1f
B. Bueno <bbueno25@hotmail.com>
2020-03-31 13:25:33 -0600

    Buffering dataset - Creating a Chatbot with Deep Learning, Python, and TensorFlow p.3

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,18 +1,18 @@
 """
 DOCSTRING
 """
-# standard
-import datetime
-# non-standard
 import json
 import sqlite3
 
-timeframe = '2015-05'
-sql_transaction = []
-connection = sqlite3.connect('{}2.db'.format(timeframe))
-c = connection.cursor()
+TIMEFRAME = '2015-05'
+SQL_TRANSACTION = []
+CONNECTION = sqlite3.connect('{}2.db'.format(TIMEFRAME))
+c = CONNECTION.cursor()
 
 def create_table():
+    """
+    DOCSTRING
+    """
     c.execute(
         "CREATE TABLE IF NOT EXISTS parent_reply( \
         parent_id TEXT PRIMARY KEY, \
@@ -23,5 +23,40 @@ def create_table():
         unix INT, \
         score INT)")
 
+def find_parent(pid):
+    """
+    DOCSTRING
+    """
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result is not None:
+            return result[0]
+        else: 
+            return False
+    except Exception as exception:
+        return False
+
+def format_data(data):
+    """
+    DOCSTRING
+    """
+    data = data.replace('\n', ' newlinechar ').replace('\r', ' newlinechar ').replace('"', "'")
+    return data
+
 if __name__ == '__main__':
     create_table()
+    ROW_COUNTER = 0
+    PAIRED_ROWS = 0
+    with open('RC_2015-01', buffering=1000) as file:
+        for row in file:
+            ROW_COUNTER += 1
+            row = json.loads(row)
+            parent_id = row['parent_id']
+            body = format_data(row['body'])
+            created_utc = row['created_utc']
+            score = row['score']
+            comment_id = row['name']
+            subreddit = row['subreddit']
+            parent_data = find_parent(parent_id)
