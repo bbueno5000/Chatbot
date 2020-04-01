e52178b154ec0adc2da947eb1940b7d46f41239a
B. Bueno <bbueno25@hotmail.com>
2020-04-01 10:15:26 -0600

    Database to Training Data - Creating a Chatbot with Deep Learning, Python, and TensorFlow p.6

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -5,12 +5,44 @@ DOCSTRING
 import datetime
 # non-standard
 import json
+import pandas
 import sqlite3
 
-TIMEFRAME = '2015-05'
-SQL_TRANSACTION = []
-CONNECTION = sqlite3.connect('{}2.db'.format(TIMEFRAME))
-c = CONNECTION.cursor()
+timeframes = '2015-05'
+sql_transaction = []
+for timeframe in timeframes:
+    connection = sqlite3.connect('{}.db'.format(timeframe))
+    c = connection.cursor()
+    limit = 5000
+    last_unix = 0
+    cur_length = limit
+    counter = 0
+    test_done = False
+    while cur_length == limit:
+        df = pd.read_sql(
+            "SELECT * FROM parent_reply WHERE unix > {} and \
+            parent NOT NULL and score > 0 ORDER BY unix ASC LIMIT {}".format(last_unix,
+                                                                             limit), connection)
+        last_unix = df.tail(1)['unix'].values[0]
+        cur_length = len(df)
+        if not test_done:
+            with open('test.from','a', encoding='utf8') as f:
+                for content in df['parent'].values:
+                    f.write(content+'\n')
+            with open('test.to','a', encoding='utf8') as f:
+                for content in df['comment'].values:
+                    f.write(str(content)+'\n')
+            test_done = True
+        else:
+            with open('train.from','a', encoding='utf8') as f:
+                for content in df['parent'].values:
+                    f.write(content+'\n')
+            with open('train.to','a', encoding='utf8') as f:
+                for content in df['comment'].values:
+                    f.write(str(content)+'\n')
+        counter += 1
+        if counter % 20 == 0:
+            print(counter*limit,'rows completed so far')
 
 def acceptable(data):
     """
